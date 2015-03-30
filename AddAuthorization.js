66d4981bb76a6e88530f41ff49512111a51fbf83
Sphelele Malo <ssmalo2@gmail.com>
2015-03-30 14:26:58 +0200

    Modifications to addAuthorizationRestriction

--- AddAuthorization.js
+++ AddAuthorization.js
@@ -3,22 +3,18 @@
  */
 
    //Connecting to the database
-    var mongoose = require("mongoose");
-    mongoose.connect('mongodb://localhost/test');
-
-    //var file = require("./dbconfig.js");
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/BuzzDB');
 
     //testing to see if connection was successfull.
      var db = mongoose.connection;
      db.on('error', console.error.bind(console, 'connection error:'));
-     db.once('open', function (callback) {
 
-         //outputs to terminal if no exceptions were thrown (success)
          //creating schema
          var Roles = new mongoose.Schema({
              ID: Number,
              Name: String
-         })
+         });
 
          //compile the schema to allow objects to be made of it
          var Role = mongoose.model('Role', Roles);
@@ -26,129 +22,70 @@
          var services = new mongoose.Schema({
              ID: String,
              details: String
-         })
+         });
          //compile the schema to allow objects to be made of it
          var Service = mongoose.model('Service', services);
 
          //creating schema
-         var BuzzSpaces = new mongoose.Schema
-         ({
-             id: String,
-             name: String,
-             code: String
-         })
-         //compile the schema to allow objects to be made of it
-         var BuzzSpace = mongoose.model('BuzzSpace', BuzzSpaces);
-
-         //...Creating models and database schemas... (starts here)
-         var restrictions = new mongoose.Schema({
-             id: String,
-             buzzSpace: [BuzzSpaces],
-             servicesID: [services],
-             minimumRole: [Roles],
-             minimumStatusPoints: Number
-         })
 
          //compile the schema to allow objects to be made of it
-         var Restriction = mongoose.model('Restriction', restrictions);
-         module.exports.mongoose = mongoose;
-         module.exports.restrict = Restriction;
-         var authorization = require("./Authorized.js");
-         //...Creating models and database schemas...(ends here)
-
-         //Getter functions for models (not really needed atm since I couldn't externalise the AddAuthor. Function)
-         /*function getRestriction()
-         {
-             return Restriction;
-         }
-         function getBuzzSpace()
-         {
-             return BuzzSpace;
-         }
-         function getRole()
-         {
-             return Role;
-         }
-         function getService()
-         {
-             return Service;
-         }*/
-
-         //get Authorized object...
-         var Authorized = authorization.Authorized;
-
-
-         //invoke the function call of addAuthorizationRestrictions
-         var user = new Role({ID:3,Name:"Admin"});
-         var person = new Role({ID:2,Name:"user"});
-         var comment = new Service({ID:"2",details:"addAuthorizationRestrictions"});
-         var buzz = new BuzzSpace({id:"2",name:"Programming Languages",code:"COS333"});
-
-         var Auth = new Authorized(user,comment,buzz); //creates the authorized object to call isAuthorized with.
-
-         addAuthorizationRestrictions(user, buzz, comment, person, 11);// calling addAuthorizationRestricions.
-
-         console.log("Success");
-
 
          //function to addAuthorizationRestricions
-         function addAuthorizationRestrictions(userRole,buzzspace,serv,minRole,minStatusPoints)
+         function addAuthorizationRestrictions(buzzspaceID, statusPoints, Role, ServiceID)
          {
-             /*Calling is Authorized to check if the user is of the
-             minimum role(Admin in this case) to use this service*/
+             //...Creating models and database schemas... (starts here)
+
+             var restrictions = new mongoose.Schema({
+                 ID: String,
+                 buzzspace_id: [mongoose.Schema.Types.ObjectID],
+                 servicesID: [mongoose.Schema.Types.ObjectID],
+                 minimumRole: [mongoose.Schema.Types.ObjectID],
+                 minimumStatusPoints: Number,
+                 deleted: Boolean
+             });
+
+             //compile the schema to allow objects to be made of it
+             var Restriction = mongoose.model('Restriction', restrictions);
 
              /*Using a call back function so that this function
              doesn't go on ahead before it is authorized to do so...*/
-             Auth.isAuthorized(function(val){
 
-                 if (val == true)  // addAuthorizationRestrictions is the service being used by the user.
-                 {
+             //this id will be unique for each buzzSpace and Service
+             var newID = buzzspaceID + ServiceID; //For now this will generate the id for restrictions...
 
-                     //console.log(buzzspace+" "+serv);
-                     //query documents
-
-                     //this id will be unique for each buzzSpace and Service
-                     var newID = buzz.code + serv.ID; //For now this will generate the id for restrictions...
-
-                     Restriction.find({'id': newID}, function (err, docs) {
-                         //console.log(docs.toString());
-                         if (docs.toString() == "") {
-                             var rest = new Restriction({
-                                 'id': newID,
-                                 'buzzSpace': buzzspace,
-                                 'servicesID': serv,
-                                 'minimumRole': minRole,
-                                 'minimumStatusPoints': minStatusPoints
-                             });
-                             rest.save(function (err, t) {
-                                 if (err) return console.error(err)
-                             });
-                             console.log("inserted");
-                         }
-                         else //updates if this restriction exists for a buzzSpace and service (is not needed since the the pre-condition isn't met.) 
-                         {
-                             //   console.log("this restriction already exists, updating now.");
-                             //   Restriction.findOne({ 'id': newID}, function (err, doc){
-                             //        doc.minimumRole = minRole;
-                             //        doc.minimumStatusPoints = minStatusPoints;
-                             //        doc.save();
-                             // });
-                             console.log("The restriction for this role in this BuzzSpace already exists."); //Pre-conditon - authorization restriction for that role and buzzSpace don't exist.
-                         }
+             Restriction.find({'id': newID}, function (err, docs) {
+                 if (docs.toString() == "") {
+                     var rest = new Restriction({
+                         'ID': newID,
+                         'buzzspace_id': buzzspaceID,
+                         'servicesID': ServiceID,
+                         'minimumRole': Role,
+                         'minimumStatusPoints': statusPoints,
+			 'deleted' : false
                      });
-
+                     rest.save(function (err, t) {
+                         if (err) return console.error(err)
+                     });
+                     console.log("inserted");
                  }
-                 else {
-                     console.log("The user isn't an Admin and doesn't have the authority to use this service.");
+                 else //restriction already exists
+                 {
+                     console.log("The restriction for this role in this BuzzSpace already exists."); //Pre-conditon - authorization restriction for that role and buzzSpace don't exist.
                  }
              });
 
+             module.exports.restrict = Restriction;
          }
+
+         exports.mongoose = mongoose;
 
          //For testing purposes a dummy isAuthorized function
          //function isAuthorized(){return true;}
 
-});
+//});
+
+exports.add = addAuthorizationRestrictions;
+
 
 
 
