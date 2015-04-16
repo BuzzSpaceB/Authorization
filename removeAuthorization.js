f00c32aa14efaf1adc9ebcd509748d194be782cd
u13049136 <u13049136@tuks.co.za>
2015-04-16 12:49:57 +0200

    removeAuth

--- removeAuthorization.js
+++ removeAuthorization.js
@@ -1,122 +1,141 @@
 //TODO use template class
+/**
+ * @module buzzAuthorisation
+ * @author AuthorisationB - Jessica Lessev
+ * @version 0.2
+ */
 
-//------------------------Defining Class-----------------------------------//
-function removeAuthorization(authorizedID)
-{
-	//Variables
-	this.authID = authorizedID;
-}
+/**
+ * Removes a users ability to use a specific service by setting a deleted flag to false.
+ * @param {String} buzzspaceName -  name of the buzz space for which Authorisation is being requested
+ * @param {String} objectName - Fully qualified name of Service being requested
+ * @param {String} objectMethod - method_name of Service 'objectName'.
+ * @throws {type} Object of a certain type are not found in the database
+ * @throws {Error} Object of a certain type is not found in the database
+ * @throws {Error} Could not establish a connection to the database
+ */
 
-module.exports.removeAuthorization = removeAuthorization;
-
-//--------------------------------------------------------------------------//
-//------------------------Adding functions--------------------------------//
-
-//Helper Function
-removeAuthorization.prototype.getID = function() 
-{
-	return this.authID;
-};
-
-//Helper Function
-removeAuthorization.prototype.setID = function(authorizedID) 
-{
-	this.authID = authorizedID;
-};
+var mongoose = require("mongoose");
 
 //Main Function
-//TODO update the authorization delete value to false if not already false. If false ignore.
-//TODO update the parameters to: (buzzspaceName, objectName, objectMethod)
-//TODO throw errors for instead of console logging errors. Console log regular outputs.
-//TODO use database stuff schemas
-//TODO remember if deleted there is a flag.
-removeAuthorization.prototype.remove = function(ID)
+//TODO update the authorization delete value to false if not already false. If false ignore. - Done
+//TODO throw errors for instead of console logging errors. Console log regular outputs. - Done
+//TODO use database stuff schemas - Done
+//TODO remember if deleted there is a flag. - Done
+exports.removeAuthorization = function removeAuthorization(/*buzzspaceName, objectName, objectMethod*/restrictionID)
 {
-    this.setID(ID);
-	//Initial connection status set to false
-	var connected = false;
-	
-	//Connecting to the database
-	var mongoose = require("mongoose");
-    mongoose.connect("mongodb://d3user:DdJXhhsd2@proximus.modulusmongo.net:27017/purYv9ib");
-
 	//Testing if database connection was successful
-	
-	//var file = require("./AddAuthorization.js");
-	//var Restriction = file.restrict;
-	//var mongoose = file.mongoose;
+	var ServiceRestrictionSchema = new mongoose.Schema(
+	{
+		restriction_id: String,
+		buzz_space_id: [mongoose.Schema.Types.ObjectID],
+		service_id: [mongoose.Schema.Types.ObjectID],
+		minimum_role: [mongoose.Schema.Types.ObjectID],
+		minimum_status_points: Number,
+		deleted: Boolean
+	});
+		
+	var Restriction = mongoose.model('servicerestrictions', ServiceRestrictionSchema);
+
+	//Connecting to the database
+	mongoose.connect("mongodb://d3user:DdJXhhsd2@proximus.modulusmongo.net:27017/purYv9ib");
 
 	var db = mongoose.connection;
 	if (db != null)
 	{
 		connected = true;
 	}
-	else
+	else 
 	{
 		connected = false;
 	}
 	
-	//Creating models and database schemas for testing
-
-	var restrictions = new mongoose.Schema(
-	{
-		 ID: String,
-		 buzzspace_id: [mongoose.Schema.Types.ObjectID],
-		 servicesID: [mongoose.Schema.Types.ObjectID],
-		 minimumRole: [mongoose.Schema.Types.ObjectID],
-		 minimumStatusPoints: Number,
-		 deleted: Boolean
-	});
-	
-	 var Restriction = mongoose.model('Restriction', restrictions);
-
 
 	if (connected == true)
 	{
-    console.log("ID : "+ID);
-    Restriction.find({'ID': ID}, function (err, docs)
-	{
+		var RoleSchema = mongoose.Schema({
+		    role_id         : String,           /* The id of the role */
+		    name            : String,            /* The name of the role, as from LDAP */
+		    role_weight     : Number            /* The weighting of the role, this is used for comparison of the roles*/
+		});
+		var Role = mongoose.model('roles', RoleSchema);
 
-		//Check to see that restriction exists
-		//TODO throw an error instead of loggin
-		if (docs.toString() == "")
+		var ServiceSchema = mongoose.Schema({
+		    service_id                  : ObjectId,
+		    service_name                : String, /*Fully qualified service name */
+		    method_name                 : String,
+		    deleted                     : Boolean
+		});
+		var Service = mongoose.model('services', ServiceSchema);
+
+		var UserSchema = mongoose.Schema({
+		    user_id             : String,           /* PK, this is the user_id as in LDAP i.e student number */
+		    username            : String,           /* The user's preferred username, like first name */
+		    roles               : [{role_name : [String], module: [String]}],      /* Array of Roles & modules of the user as from LDAP */
+		    modules      		: [String],          /* Array of Modules that is active for the user */
+		    post_count			: Number,
+		    status_value        : Number
+		});
+
+		var User = mongoose.model('users', UserSchema);
+
+		//find the user object from the userID
+		//console.log("ID : "+ID);
+		User.findOne({restriction_id: restrictionID}, function(err, _Res)
 		{
-			console.log("The restriction for this role in this BuzzSpace does not exist.");
-		}
-		//restriction exists
-		else
-		{
-			//Defining parameters to update deleted field
-			var query = { ID: ID };
-			var options = { multi: false };
-			var update =  { deleted: true };
-
-			Restriction.update(query, { $set: update}, options , callback);
-
-			function callback (error, numAffected)
+			if (err)
 			{
-			  // numAffected is the number of updated documents - should be 1
-                console.log("Num Affected: " + numAffected);
+				throw{
+				    name: "Error",
+				    message: err,
+				    toString: function()
+					{
+					return this.name + ": " + this.message;
+					}
+				}
 			}
-			console.log("removed");
-		}
-	});
+		    else 
+			if (_Res == null)
+			{
+				throw{
+				    name: "Object Not Found",
+				    message: 'Could not find Restriction in database',
+				    toString: function(){
+					return this.name + ": " + this.message;
+					}
+				}
+			}
+			else
+			{
+				if (_Res.deleted == false)
+				{
+					
+				}
+				else
+				{
+					//Defining parameters to update deleted field
+					var query = { restriction_id: restrictionID };
+					var options = { multi: false };
+					var update =  { deleted: true };
 
-	}
-	else
-	{
-	    console.log("Could not establish a connection to the database.");
-	}
+					Restriction.update(query, { $set: update}, options , callback);
 
+					function callback (error, numAffected)
+					{
+						// numAffected is the number of updated documents - should be 1
+						//console.log("Num Affected: " + numAffected);
+					}
+				}				
+			}
+		}		
+    }
+    else
+    {
+        throw{
+            name: "Connection Error",
+            message: "Could not establish a connection to the database.",
+            toString:    function(){return this.name + ": " + this.message;}
+        }
+    }
+    mongoose.disconnect();
 };
-
-//--------------------------------------------------------------------------//
-module.exports.remove = removeAuthorization;
-var id;
-
-//Creating class instance
-//var removerAuth = new removeAuthorization(id); //Need to define parameters
-
-
-// removeAuthorization.remove();
-
