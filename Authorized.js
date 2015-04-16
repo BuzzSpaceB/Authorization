bc911171d226914731e653a34050c50db925eb98
ArmandPie <u12167844@tuks.co.za>
2015-04-16 19:49:50 +0200

    Async tests working and added failing function calls

--- Authorized.js
+++ Authorized.js
@@ -371,113 +371,133 @@ exports.addAuthorizationRestrictions = function addAuthorizationRestrictions(buz
     var dbAddress = "mongodb://d3user:DdJXhhsd2@proximus.modulusmongo.net:27017/purYv9ib";
     var db = con(dbAddress);
 
+
         if (db != null) //check if connection succeeded
         {
-
-            BuzzSpace.find({'module_id': buzzspaceID}, function (err, docs) {
-                if (docs.toString() == "") //Check if the Service exists
+                function pretests()//this function tests for the 4 other prerequisites that needs to be met.
                 {
-                    throw{
-                        name: "BuzzSpace doesn't exist",
-                        message: "The selected buzzSpace doesn't exist",
-                        toString: function () {
-                            return this.name + ": " + this.message;
+                        BuzzSpace.find({'module_id': buzzspaceID}, function (err, docs) {
+                        if (docs.toString() == "") //Check if the Service exists
+                        {
+                            throw{
+                                name: "BuzzSpace doesn't exist",
+                                message: "The selected buzzSpace doesn't exist",
+                                toString: function () {
+                                    return this.name + ": " + this.message;
+                                }
+
+                            }
+                            return false;
                         }
 
-                    }
+                            Services.find({'service_id': ServiceID}, function (err, docs) {
+                                if (docs.toString() == "") //Check if the Service exists
+                                {
+                                    throw{
+                                        name: "Service Non-existant",
+                                        message: "The service specified doesn't exist",
+                                        toString: function () {
+                                            return this.name + ": " + this.message;
+                                        }
+                                    }
+                                    return false;
+                                }
+
+                                Role.find({'role_id': role.role_id}, function (err, docs) {
+                                    if (docs.toString() == "") //Check if the Service exists
+                                    {
+                                        throw{
+                                            name: "Invalid Role",
+                                            message: "The selected role doesn't exist",
+                                            toString: function () {
+                                                return this.name + ": " + this.message;
+                                            }
+                                        }
+                                        return false;
+                                    }
+
+                                    if (statusPoints < 0) //Check if the minimum statuspoints is valid
+                                    {
+                                        throw{
+                                            name: "StatusPoints Invalid",
+                                            message: "The minimus status points are less than 0",
+                                            toString: function () {
+                                                return this.name + ": " + this.message;
+                                            }
+                                        }
+                                        return false;
+                                    }
+                                    else{return true;}
+
+                                });
+
+                            });
+
+                    });
+
+
+
+
+
+
+
 
                 }
-            });
 
-            Services.find({'service_id': ServiceID}, function (err, docs) {
-                if (docs.toString() == "") //Check if the Service exists
-                {
-                    throw{
-                        name: "Service Non-existant",
-                        message: "The service specified doesn't exist",
-                        toString: function () {
-                            return this.name + ": " + this.message;
-                        }
-                    }
-
-                }
-            });
-
-            Role.find({'role_id': role.role_id}, function (err, docs) {
-                if (docs.toString() == "") //Check if the Service exists
-                {
-                    throw{
-                        name: "Invalid Role",
-                        message: "The selected role doesn't exist",
-                        toString: function () {
-                            return this.name + ": " + this.message;
-                        }
-                    }
-
-                }
-            });
-
-            if (statusPoints < 0) //Check if the minimum statuspoints is valid
+            if (pretests())
             {
-                throw{
-                    name: "StatusPoints Invalid",
-                    message: "The minimus status points are less than 0",
-                    toString: function () {
-                        return this.name + ": " + this.message;
+                //this id will be unique for each buzzSpace and Service
+                var newID = buzzspaceID + ServiceID; //For now this will generate the id for restrictions...
+
+                ServiceRestriction.find({'restriction_id': newID}, function (err, docs) { //Check if the restriction exists
+                    if (docs.toString() == "") {
+                        var rest = new ServiceRestriction({
+                            'restriction_id': newID,
+                            'buzz_space_id': buzzspaceID,
+                            'service_id': ServiceID,
+                            'minimum_role': role.role_id,
+                            'minimum_status_points': statusPoints,
+                            'deleted': false
+                        });
+                        rest.save(function (err, t) {
+                            if (err) return console.error(err)
+
+                        });
+
+
                     }
-                }
-
-            }
-
-
-            //this id will be unique for each buzzSpace and Service
-            var newID = buzzspaceID + ServiceID; //For now this will generate the id for restrictions...
-
-            ServiceRestriction.find({'restriction_id': newID}, function (err, docs) { //Check if the restriction exists
-                if (docs.toString() == "") {
-                    var rest = new ServiceRestriction({
-                        'restriction_id': newID,
-                        'buzz_space_id': buzzspaceID,
-                        'service_id': ServiceID,
-                        'minimum_role': role.role_id,
-                        'minimum_status_points': statusPoints,
-                        'deleted': false
-                    });
-                    rest.save(function (err, t) {
-                        if (err) return console.error(err)
-
-                    });
-
-
-                }
-                else //restriction already exists
-                {
-
-                    if (docs.deleted == true) // change to false
+                    else //restriction already exists
                     {
-                        var query = {restriction_id: restrictionID};
-                        var options = {multi: false};
-                        var update = {deleted: false};
-                        Restriction.update(query, {$set: update}, options, callback);
-                        function callback(error, numAffected) {
-                            console.log("Set to true")
-                        }
-                    }
-                    else {
-                        //Pre-conditon - authorization restriction for that role and buzzSpace don't exist.
-                        throw{
-                            name: "Restriction_Exists",
-                            message: "The restriction for this service on the current buzz space already exists.",
-                            toString: function () {
-                                return this.name + ": " + this.message;
+
+                        if (docs.deleted == true) // change to false
+                        {
+                            var query = {restriction_id: restrictionID};
+                            var options = {multi: false};
+                            var update = {deleted: false};
+                            Restriction.update(query, {$set: update}, options, callback);
+                            function callback(error, numAffected) {
+                                console.log("Set to true")
                             }
                         }
+                        else {
+                            //Pre-conditon - authorization restriction for that role and buzzSpace don't exist.
+                            throw{
+                                name: "Restriction_Exists",
+                                message: "The restriction for this service on the current buzz space already exists.",
+                                toString: function () {
+                                    return this.name + ": " + this.message;
+                                }
+                            }
+                            return;
+                        }
+
+
                     }
 
-
-                }
-
-            });
+                });
+            }
+            else
+            {console.log("prerequisites weren't met")}
 
 
         }
